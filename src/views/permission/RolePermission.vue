cf32dbd91a449df665fae85f9254907132cbf995
霍祖阳 <huozuyang@pconline.com.cn>
2023-10-27 16:57:21 +0800

    1

--- src/views/permission/RolePermission.vue
+++ src/views/permission/RolePermission.vue
@@ -26,9 +26,6 @@
       </el-table-column>
     </el-table>
 
-
-
-
     <el-input v-model="filterText" placeholder="Filter keyword" style="margin-bottom:30px;" />
 
     <el-tree ref="tree2" :data="data2" :props="defaultProps" :filter-node-method="filterNode" class="filter-tree"
